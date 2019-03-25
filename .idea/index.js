d3411b232e1839d5c03ab151d57e49c7c9bf0d80
enlevy <eve-krebes@web.de>
2019-03-25 23:30:07 +0100

    chat fully functional

--- .idea/index.js
+++ .idea/index.js
@@ -6,10 +6,19 @@ app.get('/',function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/script.js',function(req, res) {
+    res.sendFile(__dirname + '/script.js')
+});
+
+app.get('/style.css',function(req,res) {
+    res.sendFile(__dirname + '/style.css')
+})
+
 io.on('connection', function (socket) {
     console.log('a user connected');
-    socket.on('chat message', function(msg){
-        io.emit('chatmessage', msg);
+    socket.on('message', function(msg){
+        console.log(msg.text)
+        io.emit('broadcastmessage', msg);
     })
     socket.on('disconnect', function(){
         console.log('user disconnected');
--- .idea/script.js
+++ .idea/script.js
@@ -1,16 +1,19 @@
 
 
-
 $(function(){
-    var socket = io();
+    // Replace "localhost" with ipv4 address from ipconfig (windows) or ifconfig (mac/linux) command from
+    // console(shell), to chat via (W)LAN
+    var socket = io("http://localhost:3000");
+    socket.connect();
     $('form').submit(function(e){
         e.preventDefault(); // preventing reloading page
-        socket.emit('chat message', $('#m').val());
-    $('#m').val('');
+        socket.emit('message', {text: $('#m').val()});
+        $('#m').val('');
     return false;
     });
-    socket.on('chat message', function(msg){
-        $('#messages').append($('<li>').text(msg));
+    socket.on('broadcastmessage', function(msg){
+        console.log(msg.text)
+        $('#messages').append($('<li>').text(msg.text));
     });
 });
 
